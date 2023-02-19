d02a513058d3da84580b66faf1b0f77336ec4e9c
Andrew Loza <andrejloza81@gmail.com>
2023-02-19 19:23:33 +0200

    fist commit

---
+++ main.js
@@ -0,0 +1,13 @@
+for (let i = 4; i <= 15; i++) {
+  console.log(i);
+}
+
+for (let b = 1; b <= 8; b++) {
+  console.log("8");
+}
+
+let c = +prompt("enter number");
+
+for (let d = 1; d <= c; d++) {
+  console.log("hello");
+}
